a8843494c799caa3fb51e9f117311e345e8cffa5
ripperts <wyfabab123>
2022-08-25 13:31:58 +0800

    文档请求参数

--- src/main/java/com/upload/controller/UploadController.java
+++ src/main/java/com/upload/controller/UploadController.java
@@ -5,6 +5,8 @@ import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import com.upload.servlet.upload.FileServiceFactory;
 import com.upload.servlet.upload.UploadEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +17,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@Api(tags = "文件上传模块")
 public class UploadController {
 
     @PostMapping("/upload")
+    @ApiOperation("上传文件")
     public UploadEntity upload(@RequestPart @RequestParam("file") MultipartFile file,
                                @ApiParam("文件ID,不传为速记一个uuid") @RequestParam(required = false) String batchId,
                                @ApiParam("是否公开 1公开 0私有") @RequestParam(required = false, defaultValue = "1") String isPublic) throws IOException {
